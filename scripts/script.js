43edee7f001e2d303469f75cbca2b9ba97857a9a
anhquanpbc <anhquanc3k43pbc@gmail.com>
2024-05-27 23:02:35 +0800

    update UI

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch("../markdown/content.md")
-        .then(response => response.text())
-        .then(markdownText => {
-            const htmlContent = marked(markdownText);
-            document.getElementById('html-output').innerHTML = htmlContent;
-        })
-        .catch(error => console.error('Error fetching the markdown file:', error));
-});
+
+// Fetch the JSON file
+fetch('../configs/langs-and-tools.json')
+    .then(response => response.json())
+    .then(data => {
+        // Get the div where you want to add the icons
+        const div = document.getElementById('langs');
+
+        // Loop through the array of objects
+        data.forEach(item => {
+            // Create a new img element
+            const img = document.createElement('img');
+
+            // Set the src attribute of the img element
+            img.src = item.url;
+
+            // Set the alt attribute of the img element
+            img.alt = item.name;
+
+            // Add the img element to the div
+            div.appendChild(img);
+        });
+    })
+    .catch(error => console.error('Error:', error));
